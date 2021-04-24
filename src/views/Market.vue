3a9b7d027b7d59bd1fbc7914fd7e859e7e8377b7
teerut26 <teerutsr@gmail.com>
2021-04-24 12:55:48 +0700

    1.0.2

--- src/views/Market.vue
+++ src/views/Market.vue
@@ -1,7 +1,10 @@
 <template>
-  <b-card v-if="$store.state.showPage" tag="article" class="mb-2 mr-3 ml-3 bg-dark" >
-            <div v-if="$store.state.renderComponent" class="chart">
-                <VueTradingView :options='{
+  <div>
+    <b-row class="px-2 text-left">
+      <b-col sm>
+        <b-card v-if="$store.state.showPage" tag="article" class="mb-2 bg-dark">
+          <div v-if="$store.state.renderComponent" class="chart">
+            <VueTradingView :options='{
                     "autosize": true,
                     "symbol": "BITKUB:BTCTHB",
                     "interval": $store.state.chart_config.interval,
@@ -18,23 +21,139 @@
                     "popup_width": $store.state.chart_config.popup_width,
                     "popup_height": $store.state.chart_config.popup_height,
                 }' />
-            </div>
+          </div>
         </b-card>
+      </b-col>
+      <b-col sm>
+        <b-card class="bg-dark">
+          <b-list-group>
+            <b-list-group-item class="bg-dark  flex-column align-items-start">
+              <div class="d-flex w-100 justify-content-between" style="color: white;">
+                <h5 class="mb-1"><img src="https://www.bitkub.com/static/images/icons/BTC.png" width="30" height="30"
+                    alt="" srcset="" style="padding-bottom: 4px; padding-left: 4px;"> BTC</h5>
+                <small style="color: white;">{{numberWithCommas($store.state.crypto_data.BTC.last)}}</small>
+                <small style="color: white;"> <b
+                    :class="checkColor($store.state.crypto_data.BTC.percentChange)">{{numberPersen($store.state.crypto_data.BTC.percentChange)}}%</b>
+                  ({{numberWithCommas($store.state.crypto_data.BTC.change)}})</small>
+              </div>
+            </b-list-group-item>
+            <b-list-group-item class="bg-dark flex-column align-items-start">
+              <div class="d-flex w-100 justify-content-between" style="color: white;">
+                <h5 class="mb-1"><img src="https://www.bitkub.com/static/images/icons/ETH.png" width="30" height="30"
+                    alt="" srcset="" style="padding-bottom: 4px; padding-left: 4px;"> ETH</h5>
+                <small style="color: white;">{{numberWithCommas($store.state.crypto_data.ETH.last)}}</small>
+                <small style="color: white;"> <b
+                    :class="checkColor($store.state.crypto_data.ETH.percentChange)">{{numberPersen($store.state.crypto_data.ETH.percentChange)}}%</b>
+                  ({{numberWithCommas($store.state.crypto_data.ETH.change)}})</small>
+              </div>
+            </b-list-group-item>
+            <b-list-group-item class="bg-dark flex-column align-items-start">
+              <div class="d-flex w-100 justify-content-between" style="color: white;">
+                <h5 class="mb-1"><img src="https://www.bitkub.com/static/images/icons/XRP.png" width="30" height="30"
+                    alt="" srcset="" style="padding-bottom: 4px; padding-left: 4px;"> XRP</h5>
+                <small style="color: white;">{{numberWithCommas($store.state.crypto_data.XRP.last)}}</small>
+                <small style="color: white;"> <b
+                    :class="checkColor($store.state.crypto_data.XRP.percentChange)">{{numberPersen($store.state.crypto_data.XRP.percentChange)}}%</b>
+                  ({{numberWithCommas($store.state.crypto_data.XRP.change)}})</small>
+              </div>
+            </b-list-group-item>
+            <b-list-group-item class="bg-dark flex-column align-items-start">
+              <div class="d-flex w-100 justify-content-between" style="color: white;">
+                <h5 class="mb-1"><img src="https://www.bitkub.com/static/images/icons/BNB.png" width="30" height="30"
+                    alt="" srcset="" style="padding-bottom: 4px; padding-left: 4px;"> BNB</h5>
+                <small style="color: white;">{{numberWithCommas($store.state.crypto_data.BNB.last)}}</small>
+                <small style="color: white;"> <b
+                    :class="checkColor($store.state.crypto_data.BNB.percentChange)">{{numberPersen($store.state.crypto_data.BNB.percentChange)}}%</b>
+                  ({{numberWithCommas($store.state.crypto_data.BNB.change)}})</small>
+              </div>
+            </b-list-group-item>
+            <b-list-group-item class="bg-dark flex-column align-items-start">
+              <div class="d-flex w-100 justify-content-between" style="color: white;">
+                <h5 class="mb-1"><img src="https://www.bitkub.com/static/images/icons/DOGE.png" width="30" height="30"
+                    alt="" srcset="" style="padding-bottom: 4px; padding-left: 4px;"> DOGE</h5>
+                <small style="color: white;">{{numberWithCommas($store.state.crypto_data.DOGE.last)}}</small>
+                <small style="color: white;"> <b
+                    :class="checkColor($store.state.crypto_data.DOGE.percentChange)">{{numberPersen($store.state.crypto_data.DOGE.percentChange)}}%</b>
+                  ({{numberWithCommas($store.state.crypto_data.DOGE.change)}})</small>
+              </div>
+            </b-list-group-item>
+            <b-list-group-item class="bg-dark flex-column align-items-start">
+              <div class="d-flex w-100 justify-content-between" style="color: white;">
+                <h5 class="mb-1"><img src="https://www.bitkub.com/static/images/icons/USDT.png" width="30" height="30"
+                    alt="" srcset="" style="padding-bottom: 4px; padding-left: 4px;"> USDT</h5>
+                <small style="color: white;">{{numberWithCommas($store.state.crypto_data.USDT.last)}}</small>
+                <small style="color: white;"> <b
+                    :class="checkColor($store.state.crypto_data.USDT.percentChange)">{{numberPersen($store.state.crypto_data.USDT.percentChange)}}%</b>
+                  ({{numberWithCommas($store.state.crypto_data.USDT.change)}})</small>
+              </div>
+            </b-list-group-item>
+          </b-list-group>
+        </b-card>
+      </b-col>
+    </b-row>
+
+  </div>
+
 </template>
 
 <script>
-    import firebase from '../firebase.js'
-    const Swal = require('sweetalert2')
-    import VueTradingView from 'vue-trading-view';
+  import firebase from '../firebase.js'
+  const Swal = require('sweetalert2')
+  import VueTradingView from 'vue-trading-view';
 
-    export default {
-        name: 'HelloWorld',
-        props: {
-            msg: String
-        },
-        components: {
-            VueTradingView,
-        },
+  export default {
+    name: 'HelloWorld',
+    data() {
+      return {
+        items: [{
+            age: 40,
+            first_name: 'Dickerson',
+            last_name: 'Macdonald'
+          },
+          {
+            age: 21,
+            first_name: 'Larsen',
+            last_name: 'Shaw'
+          },
+          {
+            age: 89,
+            first_name: 'Geneva',
+            last_name: 'Wilson'
+          },
+          {
+            age: 38,
+            first_name: 'Jami',
+            last_name: 'Carney'
+          }
+        ]
+      }
+    },
+    methods: {
+      numberWithCommas(x) {
+        var num2 = x.toFixed(2)
+        return parseFloat(num2).toLocaleString()
+      },
+      numberPersen(x) {
+        // return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+        if (x < 0) {
+          var num1 = x.toFixed(2)
+          return parseFloat(num1).toLocaleString()
+        } else if (x > 0) {
+          var num2 = x.toFixed(2)
+          return '+' + parseFloat(num2).toLocaleString()
+        }
+      },
+      checkColor(number) {
+        if (number < 0) {
+          return "text-danger"
+        } else {
+          return "text-success"
+        }
+      }
+    },
+    components: {
+      VueTradingView,
+    },
     mounted() {
       this.$store.state.titleBar = "Home"
       firebase.database().ref('users/' + localStorage.getItem("username")).on('value', (snapshot) => {
@@ -66,16 +185,26 @@
         }
       })
     },
-    }
+  }
 </script>
 <style>
-    .chart{
-        height: 550px;
-    }
-    #vue-trading-view{
-        height: 100%;
-    }
-    .card-body{
-        padding: 20px;
-    }
+  .chart {
+    height: 550px;
+  }
+
+  #vue-trading-view {
+    height: 100%;
+  }
+
+  .card-body {
+    padding: 20px;
+  }
+
+  .list-group-item {
+    padding: 10px;
+  }
+
+  .d-flex {
+    height: 24px;
+  }
 </style>
